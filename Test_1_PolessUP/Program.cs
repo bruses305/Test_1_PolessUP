02739ce453bde86956805c58692958bcd7aaddcd
37533 <37533@LAPTOP-GAM2DSJ2>
2023-04-21 20:39:46 +0300

    Решение

--- Test_1_PolessUP/Program.cs
+++ Test_1_PolessUP/Program.cs
@@ -6,7 +6,40 @@ namespace Test_1_PolessUP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] nums = new int[] {2,5,3,1};
+            int min = nums[0];
+            for(int i = 0; i<nums.Length;i++)
+                if(min>nums[i])
+                {
+                    min = nums[i];
+                }
+            int max_1 = min;
+            int max_2 = min;
+            int max_3 = min;
+            for(int i = 0; i<nums.Length;i++)
+            {
+                if(max_1<= nums[i])
+                {
+                    max_1 = nums[i];
+                }
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (max_2 <= nums[i] && nums[i]<max_1)
+                {
+                    max_2 = nums[i];
+                }
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (max_3 <= nums[i] && nums[i]<max_2)
+                {
+                    max_3 = nums[i];
+                }
+            }
+            if(max_3!=max_2) Console.WriteLine(max_3);
+            else if(max_2!=max_1) Console.WriteLine(max_2);
+            else Console.WriteLine(max_1);
         }
     }
 }
